1fc4e107bace99e2a68743f1fd2d19b7b3d8adbc
Yoohyuk-Lim <dbgur3065@gmail.com>
2022-11-25 21:44:03 +0900

    지도는 움직일 수 있게

--- scripts/kakao_api.js
+++ scripts/kakao_api.js
@@ -1,15 +1,15 @@
 const position = new kakao.maps.LatLng(37.4836, 127.01787);
+const mapContainer = document.getElementById('map');
+const mapOption = { 
+        center: position,
+        level: 3
+    };    
+const map = new kakao.maps.Map(mapContainer, mapOption);
 const marker = new kakao.maps.Marker({
     position: position, 
     text: '더화이트베일'
 });
-const mapContainer = document.getElementById('map');
-const mapOption = { 
-        center: position,
-        level: 3,
-        marker: marker
-    };    
-const map = new kakao.maps.StaticMap(mapContainer, mapOption);
+marker.setMap(map);
 
 Kakao.init("d0c902fa29b5b46643c9124679b42bb8");
 const kakaoNavi = document.getElementById('kakao_navi');
